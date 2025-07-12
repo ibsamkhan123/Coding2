eeeb9ccee576d6cdcd1f89b513b30cb729e78b0a
ibsamkhan123 <ibsamkhan1@gmail.com>
2025-07-12 14:42:24 +0200

    gyyyyyyyyyyh

---
+++ .vscode/script1.js
@@ -0,0 +1,4 @@
+document.getElementById("result").innerHTML =
+document.getElementById("result1").firstChild.nodeValue;
+document.getElementById("result").innerHTML =
+    document.getElementById("result1").childNodes[0].nodeValue;
